cc66b0175fcac95d130f5b48fd090782ab08253f
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-11-06 14:52:55 -0600

    Feat: secured profile

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,16 +6,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        String user = (String) session.getAttribute("user");
-
-        if (user == null){
-            response.sendRedirect("/login");
-        } else {
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        }
+        session.invalidate();
+        response.sendRedirect("/login");
     }
 }
